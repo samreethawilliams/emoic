2f085f184089608194de788ca0b420f9cd5c33a2
Haari Sushil Raj <hsushilr2000@gmail.com>
2024-06-12 13:55:52 -0400

    SignUp and Login Page logo add

--- frontend/app/Login.js
+++ frontend/app/Login.js
@@ -1,10 +1,9 @@
-import { View, Text, TextInput, TouchableOpacity, Button } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react'
-import {Ionicons} from '@expo/vector-icons';
-import Checkbox from 'expo-checkbox'
-import { useNavigation } from '@react-navigation/native';
-
+import { View, Text, TextInput, TouchableOpacity, Image } from "react-native";
+import { Button } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import { useNavigation } from "@react-navigation/native";
 
 const Login = () => {
   const [isPasswordShown, setIsPasswordShown] = useState(false);
@@ -12,97 +11,108 @@ const Login = () => {
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:'#fffaf0'}}>
-      <View style={{flex:1, marginHorizontal:22}}>
-          
-          
-              <View style={{marginBottom:12}}>
-                <Text style={{
-                  fontSize:16,
-                  fontWeight:400,
-                  marginVertical:8
-                  }}>Email address</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f8f8ff" }}>
+      <View style={{ flex: 1, marginHorizontal: 22 }}>
+        <View style={{ marginBottom: 12, alignItems: "center" }}>
+          <Image
+            source={require("../assets/images/emonic_logo.png")}
+            style={{ width: 282, height: 280, marginBottom: 16 }}
+          />
+        </View>
 
-                <View style={{
-                    width:"100%",
-                    height:48,
-                    borderWidth:1,
-                    borderRadius:8,
-                    alignItems:"center",
-                    justifyContent:"center",
-                    paddingLeft:22
-                }}>
-                  <TextInput
-                      placeholder='Enter your email address'
-                      placeholderTextColor={'#808080'}
-                      keyboardType='email-address'
-                        style={{
-                          width:"100%"
-                        }}
-                      />
-                </View>  
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Email address
+          </Text>
 
-              </View>
-
-              <View style={{marginBottom:12}}>
-                <Text style={{
-                  fontSize:16,
-                  fontWeight:400,
-                  marginVertical:8
-                  }}>Password</Text>
-
-                <View style={{
-                    width:"100%",
-                    height:48,
-                    borderWidth:1,
-                    borderRadius:8,
-                    alignItems:"center",
-                    justifyContent:"center",
-                    paddingLeft:22
-                }}>
-                  <TextInput
-                      placeholder='Enter your password'
-                      placeholderTextColor={'#808080'}
-                      secureTextEntry={isPasswordShown}
-                        style={{
-                          width:"100%"
-                        }}
-                      />
-
-                      <TouchableOpacity 
-                          onPress={()=>setIsPasswordShown(!isPasswordShown)}
-                          style={{
-                          position:"absolute",
-                          right:12
-                      }}>
-                      {
-                        isPasswordShown == true? (
-                          <Ionicons name="eye-off" size={24} color={'#808080'}/>
-                        ) : (
-                          <Ionicons name="eye" size={24} color={'#808080'}/>
-                        )
-                      }
-
-                      </TouchableOpacity>
-
-                </View>  
-
-              </View>
-
-            <Button
-              title='Login'
-              onPress={() => navigation.navigate('Login')}
-              //filled
-              color={'#2f4f4f'}
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your email address"
+              placeholderTextColor={"#808080"}
+              keyboardType="email-address"
               style={{
-                marginTop:18,
-                marginBottom:4
+                width: "100%",
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Password
+          </Text>
+
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your password"
+              placeholderTextColor={"#808080"}
+              secureTextEntry={isPasswordShown}
+              style={{
+                width: "100%",
               }}
             />
 
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: "absolute",
+                right: 12,
+              }}
+            >
+              {isPasswordShown == true ? (
+                <Ionicons name="eye-off" size={24} color={"#808080"} />
+              ) : (
+                <Ionicons name="eye" size={24} color={"#808080"} />
+              )}
+            </TouchableOpacity>
+          </View>
         </View>
-    </SafeAreaView>
-  )
-}
 
-export default Login
+        <Button
+          title="Login"
+          onPress={() => navigation.navigate("Login")}
+          //filled
+          color={"#2f4f4f"}
+          style={{
+            marginTop: 18,
+            marginBottom: 4,
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default Login;
--- frontend/app/SignUp.js
+++ frontend/app/SignUp.js
@@ -1,196 +1,221 @@
-import { View, Text, TextInput, TouchableOpacity, Button } from 'react-native'
-import { SafeAreaView } from 'react-native-safe-area-context';
-import React, { useState } from 'react'
-import {Ionicons} from '@expo/vector-icons';
-import Checkbox from 'expo-checkbox'
-import { useNavigation } from '@react-navigation/native';
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Button,
+  Image,
+} from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import React, { useState } from "react";
+import { Ionicons } from "@expo/vector-icons";
+import Checkbox from "expo-checkbox";
+import { useNavigation } from "@react-navigation/native";
 import Login from "./Login";
 
 const SignUp = () => {
-
   const [isPasswordShown, setIsPasswordShown] = useState(false);
   const [isChecked, setIsChecked] = useState(false);
 
   const navigation = useNavigation();
 
   return (
-    <SafeAreaView style={{flex:1, backgroundColor:'#fffaf0'}}>
-      <View style={{flex:1, marginHorizontal:22}}>
-          
-              <View style={{marginBottom:12}}>
-                    <Text style={{
-                      fontSize:16,
-                      fontWeight:400,
-                      marginVertical:8
-                      }}>Name</Text>
+    <SafeAreaView style={{ flex: 1, backgroundColor: "#f8f8ff" }}>
+      <View style={{ flex: 1, marginHorizontal: 22 }}>
+        <View style={{ marginBottom: 6, alignItems: "center" }}>
+          <Image
+            source={require("../assets/images/emonic_logo.png")}
+            style={{ width: 182, height: 180, marginBottom: 5 }}
+          />
+        </View>
 
-                    <View style={{
-                        width:"100%",
-                        height:48,
-                        borderWidth:1,
-                        borderRadius:8,
-                        alignItems:"center",
-                        justifyContent:"center",
-                        paddingLeft:22
-                    }}>
-                        <TextInput
-                          placeholder='Enter your name'
-                          placeholderTextColor={'#808080'}
-                          keyboardType='name'
-                            style={{
-                              width:"100%"
-                            }}
-                          />
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Name
+          </Text>
 
-                    </View>  
-
-                  </View>
-          
-          
-              <View style={{marginBottom:12}}>
-                <Text style={{
-                  fontSize:16,
-                  fontWeight:400,
-                  marginVertical:8
-                  }}>Email address</Text>
-
-                <View style={{
-                    width:"100%",
-                    height:48,
-                    borderWidth:1,
-                    borderRadius:8,
-                    alignItems:"center",
-                    justifyContent:"center",
-                    paddingLeft:22
-                }}>
-                  <TextInput
-                      placeholder='Enter your email address'
-                      placeholderTextColor={'#808080'}
-                      keyboardType='email-address'
-                        style={{
-                          width:"100%"
-                        }}
-                      />
-                </View>  
-
-              </View>
-
-              <View style={{marginBottom:12}}>
-                <Text style={{
-                  fontSize:16,
-                  fontWeight:400,
-                  marginVertical:8
-                  }}>Password</Text>
-
-                <View style={{
-                    width:"100%",
-                    height:48,
-                    borderWidth:1,
-                    borderRadius:8,
-                    alignItems:"center",
-                    justifyContent:"center",
-                    paddingLeft:22
-                }}>
-                  <TextInput
-                      placeholder='Enter your password'
-                      placeholderTextColor={'#808080'}
-                      secureTextEntry={isPasswordShown}
-                        style={{
-                          width:"100%"
-                        }}
-                      />
-
-                      <TouchableOpacity 
-                          onPress={()=>setIsPasswordShown(!isPasswordShown)}
-                          style={{
-                          position:"absolute",
-                          right:12
-                      }}>
-                      {
-                        isPasswordShown == true? (
-                          <Ionicons name="eye-off" size={24} color={'#808080'}/>
-                        ) : (
-                          <Ionicons name="eye" size={24} color={'#808080'}/>
-                        )
-                      }
-
-                      </TouchableOpacity>
-
-                </View>  
-
-              </View>
-
-              <View style={{marginBottom:12}}>
-                <Text style={{
-                  fontSize:16,
-                  fontWeight:400,
-                  marginVertical:8
-                  }}>Confirm Password</Text>
-
-                <View style={{
-                    width:"100%",
-                    height:48,
-                    borderWidth:1,
-                    borderRadius:8,
-                    alignItems:"center",
-                    justifyContent:"center",
-                    paddingLeft:22
-                }}>
-                  <TextInput
-                      placeholder='Enter your password'
-                      placeholderTextColor={'#808080'}
-                      secureTextEntry={isPasswordShown}
-                        style={{
-                          width:"100%"
-                        }}
-                      />
-
-                      <TouchableOpacity 
-                          onPress={()=>setIsPasswordShown(!isPasswordShown)}
-                          style={{
-                          position:"absolute",
-                          right:12
-                      }}>
-                      {
-                        isPasswordShown == true? (
-                          <Ionicons name="eye-off" size={24} color={'#808080'}/>
-                        ) : (
-                          <Ionicons name="eye" size={24} color={'#808080'}/>
-                        )
-                      }
-
-                      </TouchableOpacity>
-
-                </View>  
-
-              </View>
-          <View style={{
-            flexDirection: 'row',
-            marginVertical:6
-          }}>
-            <Checkbox
-              style={{marginRight:8}}
-              value={isChecked}
-              onValueChange={setIsChecked}
-              color={isChecked ? '#2f4f4f': undefined}
-            />
-              <Text> I agree to the terms and conditions</Text>
-          </View>    
-
-            <Button
-              title='Sign Up'
-              onPress={() => navigation.navigate('Login')}
-              //filled
-              color={'#2f4f4f'}
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your name"
+              placeholderTextColor={"#808080"}
+              keyboardType="name"
               style={{
-                marginTop:18,
-                marginBottom:4
+                width: "100%",
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Email address
+          </Text>
+
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your email address"
+              placeholderTextColor={"#808080"}
+              keyboardType="email-address"
+              style={{
+                width: "100%",
+              }}
+            />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Password
+          </Text>
+
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your password"
+              placeholderTextColor={"#808080"}
+              secureTextEntry={isPasswordShown}
+              style={{
+                width: "100%",
               }}
             />
 
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: "absolute",
+                right: 12,
+              }}
+            >
+              {isPasswordShown == true ? (
+                <Ionicons name="eye-off" size={24} color={"#808080"} />
+              ) : (
+                <Ionicons name="eye" size={24} color={"#808080"} />
+              )}
+            </TouchableOpacity>
+          </View>
         </View>
-    </SafeAreaView>
-  )
-}
 
-export default SignUp
+        <View style={{ marginBottom: 12 }}>
+          <Text
+            style={{
+              fontSize: 16,
+              fontWeight: 400,
+              marginVertical: 8,
+            }}
+          >
+            Confirm Password
+          </Text>
+
+          <View
+            style={{
+              width: "100%",
+              height: 48,
+              borderWidth: 1,
+              borderRadius: 8,
+              alignItems: "center",
+              justifyContent: "center",
+              paddingLeft: 22,
+            }}
+          >
+            <TextInput
+              placeholder="Enter your password"
+              placeholderTextColor={"#808080"}
+              secureTextEntry={isPasswordShown}
+              style={{
+                width: "100%",
+              }}
+            />
+
+            <TouchableOpacity
+              onPress={() => setIsPasswordShown(!isPasswordShown)}
+              style={{
+                position: "absolute",
+                right: 12,
+              }}
+            >
+              {isPasswordShown == true ? (
+                <Ionicons name="eye-off" size={24} color={"#808080"} />
+              ) : (
+                <Ionicons name="eye" size={24} color={"#808080"} />
+              )}
+            </TouchableOpacity>
+          </View>
+        </View>
+        <View
+          style={{
+            flexDirection: "row",
+            marginVertical: 6,
+          }}
+        >
+          <Checkbox
+            style={{ marginRight: 8 }}
+            value={isChecked}
+            onValueChange={setIsChecked}
+            color={isChecked ? "#2f4f4f" : undefined}
+          />
+          <Text> I agree to the terms and conditions</Text>
+        </View>
+
+        <Button
+          title="Sign Up"
+          onPress={() => navigation.navigate("Login")}
+          //filled
+          color={"#2f4f4f"}
+          style={{
+            marginTop: 18,
+            marginBottom: 4,
+          }}
+        />
+      </View>
+    </SafeAreaView>
+  );
+};
+
+export default SignUp;
