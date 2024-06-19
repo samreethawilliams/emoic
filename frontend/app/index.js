64079c92f831180b37769ae00eed5c2024490b08
samreethawilliams <samreethawilliams@gmail.com>
2024-06-19 13:09:23 -0400

    Updating alignment

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -7,18 +7,31 @@ import { useNavigation } from "@react-navigation/native";
 const Home = () => {
   const navigation = useNavigation();
   return (
-    <View style= {{flex: 1 alignItems: "center", justifyContent: "center", padding: 16,}}>
+    <View
+      style={{
+        flex: 1,
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 16,
+      }}
+    >
       <View style={{ marginBottom: 100, alignItems: "center" }}>
         <EmoicLogo style={{ width: 100, height: 100 }} />
       </View>
-      <Text style={{ fontSize: 32, fontWeight: "semibold", textAlign: "center", marginBottom: 50}}>
+      <Text
+        style={{
+          fontSize: 32,
+          fontWeight: "semibold",
+          textAlign: "center",
+          marginBottom: 50,
+        }}
+      >
         Let the music reflect the emotions..
       </Text>
       <Button
         title="Continue"
         onPress={() => navigation.navigate("Login")}
         color={"3E8B9A"}
-        
       />
     </View>
   );
