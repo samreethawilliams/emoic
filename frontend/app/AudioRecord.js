b1ccecae8436d98c4a633ff9bc635b34504d5e8e
Haari Sushil Raj <hsushilr2000@gmail.com>
2024-07-03 13:02:43 -0400

    Audio rec page implemented

--- frontend/app/AudioRecord.js
+++ frontend/app/AudioRecord.js
@@ -1,12 +1,116 @@
-import { View, Text } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Audio } from "expo-av";
 
 const AudioRecord = () => {
+  const [recording, setRecording] = useState();
+  const [recordings, setRecordings] = useState([]);
+
+  async function startRecording() {
+    try {
+      const perm = await Audio.requestPermissionsAsync();
+      if (perm.status === "granted") {
+        await Audio.setAudioModeAsync({
+          allowsRecordingIOS: true,
+          playsInSilentModeIOS: true,
+        });
+        const { recording } = await Audio.Recording.createAsync(
+          Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY,
+        );
+        setRecording(recording);
+      }
+    } catch (err) {
+      console.error("Failed to start recording", err);
+    }
+  }
+
+  async function stopRecording() {
+    setRecording(undefined);
+
+    await recording.stopAndUnloadAsync();
+    let allRecordings = [...recordings];
+    const { sound, status } = await recording.createNewLoadedSoundAsync();
+    await sound.setVolumeAsync(1.0); // Set volume to maximum
+    allRecordings.push({
+      sound: sound,
+      duration: getDurationFormatted(status.durationMillis),
+      file: recording.getURI(),
+    });
+
+    setRecordings(allRecordings);
+  }
+
+  function getDurationFormatted(milliseconds) {
+    const minutes = Math.floor(milliseconds / 1000 / 60);
+    const seconds = Math.round((milliseconds / 1000) % 60);
+    return seconds < 10 ? `${minutes}:0${seconds}` : `${minutes}:${seconds}`;
+  }
+
+  function getRecordingLines() {
+    return recordings.map((recordingLine, index) => (
+      <View key={index} style={styles.row}>
+        <Text style={styles.fill}>
+          Recording #{index + 1} | {recordingLine.duration}
+        </Text>
+        <Button
+          onPress={() => handlePlay(recordingLine.sound)}
+          title="Play"
+        ></Button>
+      </View>
+    ));
+  }
+
+  async function handlePlay(sound) {
+    try {
+      await Audio.setAudioModeAsync({
+        allowsRecordingIOS: false,
+        staysActiveInBackground: true,
+        playsInSilentModeIOS: true,
+      });
+      await sound.setVolumeAsync(1.0); // Set volume to maximum when playing
+      await sound.replayAsync(); // Ensure it plays from the beginning each time
+    } catch (error) {
+      console.error("Failed to play sound", error);
+    }
+  }
+
+  function clearRecordings() {
+    setRecordings([]);
+  }
+
   return (
-    <View>
-      <Text>AudioRecord</Text>
+    <View style={styles.container}>
+      <Button
+        title={recording ? "Stop Recording" : "Start Recording\n\n\n"}
+        onPress={recording ? stopRecording : startRecording}
+      />
+      {getRecordingLines()}
+      <Button
+        title={recordings.length > 0 ? "\n\n\nClear Recordings" : ""}
+        onPress={clearRecordings}
+      />
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  row: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    marginLeft: 10,
+    marginRight: 40,
+  },
+  fill: {
+    flex: 1,
+    margin: 15,
+  },
+});
 
 export default AudioRecord;
