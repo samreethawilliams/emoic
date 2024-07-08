3c2880a57938f1b69dfe18e77d83173b09a61189
samreethawilliams <samreethawilliams@gmail.com>
2024-07-08 15:23:13 -0400

    make bg adjustments using expo-linear gradient

--- frontend/app/Player.js
+++ frontend/app/Player.js
@@ -1,3 +1,4 @@
+import React, { useEffect, useState } from "react";
 import {
   View,
   Text,
@@ -6,13 +7,12 @@ import {
   Button,
   ScrollView,
 } from "react-native";
-import React, { useEffect, useState } from "react";
 import Icon from "react-native-vector-icons/FontAwesome";
-import Footer from "./components/footer";
 import { useNavigation } from "@react-navigation/native";
 import Slider from "@react-native-community/slider";
 import { Audio } from "expo-av";
-import Bg from "./svgs/Bg";
+import { LinearGradient } from "expo-linear-gradient";
+import Footer from "./components/footer";
 
 const Player = () => {
   const navigation = useNavigation();
@@ -86,92 +86,105 @@ const Player = () => {
   };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        marginTop: 50,
-        marginBottom: 50,
-        backgroundColor: "#F0F0F0",
-        borderRadius: 30,
-      }}
+    <LinearGradient
+      colors={["#3E8B9A", "#F0F0F0"]}
+      style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
     >
-      <Bg style={{ height: 300, width: 300 }} />
-      <Image
-        source={{
-          uri: "https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_640.png",
-        }}
-        style={{ width: 200, height: 200, borderRadius: 100 }}
-      />
-      <View style={{ marginTop: 30, marginBottom: 20 }}>
-        <Text style={{ fontSize: 20, fontWeight: "bold" }}>
-          Name of the Audio
-        </Text>
-      </View>
-      <View style={{ marginBottom: 20 }}>
-        <Text>Artist</Text>
-      </View>
-      <Slider
-        style={{ width: 300, height: 50, marginLeft: 20, marginRight: 20 }}
-        minimumValue={0}
-        maximumValue={1}
-        value={getSliderValue()}
-        minimumTrackTintColor="#FFFFFF"
-        maximumTrackTintColor="#3E8B9A"
-        disabled={true}
-      />
       <View
         style={{
-          flexDirection: "row",
-          justifyContent: "space-between",
-          width: 300,
-          marginLeft: 20,
-          marginRight: 20,
-        }}
-      >
-        <Text>{getCurrentTime()}</Text>
-        <Text>{getDuration()}</Text>
-      </View>
-      <TouchableOpacity
-        style={{
-          backgroundColor: "#3E8B9A",
-          borderRadius: 100,
+          flex: 1,
+          alignItems: "center",
+          marginTop: 50,
+          marginBottom: 50,
+          borderRadius: 30,
           padding: 20,
-          marginBottom: 10,
-        }}
-        onPress={playPauseSound}
-      >
-        <Icon name={isPlaying ? "pause" : "play"} size={30} color="#fff" />
-      </TouchableOpacity>
-      <View style={{ alignItems: "center" }}>
-        <Button
-          title="Go to Dashboard"
-          onPress={() => navigation.navigate("Dashboard")}
-          color="#3E8B9A"
-        />
-      </View>
-      <ScrollView
-        style={{
-          marginTop: 30,
-          backgroundColor: "white",
-          borderRadius: 20,
-          padding: 50,
-          height: 300,
         }}
       >
-        <Text
+        <View
           style={{
-            fontSize: 14,
-            textAlign: "center",
+            backgroundColor: "white",
+            borderRadius: 30,
+            padding: 20,
+            alignItems: "center",
           }}
         >
-          {lyrics}
-        </Text>
-      </ScrollView>
-      <View style={{ alignItems: "center", marginTop: 120 }}>
-        <Footer />
+          <Image
+            source={{
+              uri: "https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_640.png",
+            }}
+            style={{ width: 200, height: 200, borderRadius: 100 }}
+          />
+        </View>
+        <View style={{ marginTop: 30, marginBottom: 20 }}>
+          <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+            Name of the Audio
+          </Text>
+        </View>
+        <View style={{ marginBottom: 20 }}>
+          <Text>Artist</Text>
+        </View>
+        <Slider
+          style={{ width: 300, height: 50, marginLeft: 20, marginRight: 20 }}
+          minimumValue={0}
+          maximumValue={1}
+          value={getSliderValue()}
+          minimumTrackTintColor="#FFFFFF"
+          maximumTrackTintColor="#3E8B9A"
+          disabled={true}
+        />
+        <View
+          style={{
+            flexDirection: "row",
+            justifyContent: "space-between",
+            width: 300,
+            marginLeft: 20,
+            marginRight: 20,
+          }}
+        >
+          <Text>{getCurrentTime()}</Text>
+          <Text>{getDuration()}</Text>
+        </View>
+        <TouchableOpacity
+          style={{
+            backgroundColor: "#3E8B9A",
+            borderRadius: 100,
+            padding: 20,
+            marginBottom: 10,
+          }}
+          onPress={playPauseSound}
+        >
+          <Icon name={isPlaying ? "pause" : "play"} size={30} color="#fff" />
+        </TouchableOpacity>
+        <View style={{ alignItems: "center" }}>
+          <Button
+            title="Go to Dashboard"
+            onPress={() => navigation.navigate("Dashboard")}
+            color="#3E8B9A"
+          />
+        </View>
+        <ScrollView
+          style={{
+            marginTop: 30,
+            backgroundColor: "white",
+            borderRadius: 20,
+            padding: 50,
+            height: 300,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: 14,
+              textAlign: "center",
+            }}
+          >
+            {lyrics}
+          </Text>
+        </ScrollView>
+        <View style={{ alignItems: "center", marginTop: 120 }}>
+          <Footer />
+        </View>
       </View>
-    </View>
+    </LinearGradient>
   );
 };
 
