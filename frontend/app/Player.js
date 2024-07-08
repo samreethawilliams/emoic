5346960f33df208a3b36cd81c99b1d5923f1c705
samreethawilliams <samreethawilliams@gmail.com>
2024-07-08 00:38:57 -0400

    modifying player

--- frontend/app/Player.js
+++ frontend/app/Player.js
@@ -5,16 +5,26 @@ import Footer from "./components/footer";
 import { useNavigation } from "@react-navigation/native";
 import Dashboard from "./Dashboard";
 import Slider from "@react-native-community/slider";
+import Bg from "./svgs/Bg";
 
 const Player = () => {
   const navigation = useNavigation();
+  const lyrics =
+    "Twinkle, twinkle, little star,How I wonder what you are!Up above the world so high,Like a diamond in the sky.";
   return (
     <View
-      style={{ flex: 1, alignItems: "center", marginTop: 50, marginBottom: 50 }}
+      style={{
+        flex: 1,
+        alignItems: "center",
+        marginTop: 50,
+        marginBottom: 50,
+        backgroundColor: "#F0F0F0",
+        borderRadius: 30,
+      }}
     >
       <Image
         source={{
-          uri: "https://rocketium.com/is/wp-content/uploads/2021/04/undraw_compose_music_ovo2.png",
+          uri: "https://cdn.pixabay.com/photo/2017/03/05/21/55/emoticon-2120024_640.png",
         }}
         style={{ width: 200, height: 200, borderRadius: 100 }}
       />
@@ -38,7 +48,21 @@ const Player = () => {
         maximumValue={1}
         minimumTrackTintColor="#FFFFFF"
         maximumTrackTintColor="#000000"
+        disabled={true}
       />
+      <Text
+        style={{
+          marginTop: 30,
+          paddingHorizontal: 10,
+          paddingVertical: 10,
+          backgroundColor: "#3E8B9A",
+          borderRadius: 20,
+          fontSize: 14,
+        }}
+      >
+        {lyrics}
+      </Text>
+      <Bg style={{ height: 20, width: 20 }} />
       <View style={{ marginTop: 50, alignItems: "center" }}>
         <Button
           title="Go to Dashboard"
