b420ae5b0c06f4690a380b56e262d28f18e9bbc1
Joel Mathew Koshy <joelmathewkoshy@gmail.com>
2024-07-09 13:13:21 -0400

    login signup apis with ngrok

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,17 @@ import express from "express";
 import cors from "cors";
 import multer from "multer";
 import { sync } from "cross-spawn";
+import ngrok from "@ngrok/ngrok";
+import pg from "pg";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+
+// database connection
+const { Client } = pg;
+const connectionString = "postgresql://postgres:postgres@localhost:5432/emoic";
+
+const client = new Client({ connectionString });
+await client.connect();
 
 const storage = multer.diskStorage({
   filename: function (req, file, cb) {
@@ -19,6 +30,7 @@ const app = express();
 const port = 8080;
 
 app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
   res.json("Emoic says hello!!");
@@ -53,6 +65,106 @@ app.post("/upload_files", upload.single("file"), async (req, res) => {
   }
 });
 
+// sign up api
+app.post("/sign-up", async (req, res) => {
+  const { name, email, password } = req.body;
+  if (!name || !email || !password) {
+    return res.send({
+      status: false,
+      message: "Required sign up fields missing",
+    });
+  }
+
+  const hashedPassword = await argon2.hash(password);
+
+  try {
+    await client.query(
+      `
+      insert into users
+      (name, email, password)
+      values
+      ($1, $2, $3)`,
+      [name, email, hashedPassword]
+    );
+
+    return res.send({
+      status: true,
+      token: jwt.sign({ name, email }, "emoic"),
+      message: "User has been registered successfully",
+    });
+  } catch (err) {
+    if (err.code === "23505") {
+      return res.send({
+        status: false,
+        message: "User is already registered",
+      });
+    }
+    console.error(err);
+  }
+});
+
+// login api
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.send({
+      status: false,
+      message: "Required login fields missing",
+    });
+  }
+
+  let user;
+
+  try {
+    //select * from users where email='bob@bob.com'
+    const tempRes = await client.query(
+      `
+      select * from users
+      where email=$1
+      `,
+      [email]
+    );
+    user = tempRes.rows[0];
+  } catch (err) {
+    console.log(err);
+    return res.send({
+      status: false,
+      message: "Some error occurred",
+    });
+  }
+
+  if (!user) {
+    return res.send({
+      status: false,
+      message: "User does not exist",
+    });
+  }
+
+  const valid = await argon2.verify(user.password, password);
+
+  if (!valid) {
+    return res.send({
+      status: false,
+      message: "Invalid credentials",
+    });
+  }
+
+  return res.send({
+    status: true,
+    token: jwt.sign({ name: user.name, email }, "emoic"),
+  });
+});
+
 app.listen(port, () => {
   console.log(`Upload server listening at http://localhost:${port}`);
 });
+
+// ngrok
+if (process.env.USE_NGROK === true) {
+  ngrok
+    .connect({ addr: port, authtoken_from_env: true })
+    .then((listener) =>
+      console.log(`Ingress established at: ${listener.url()}`)
+    );
+}
