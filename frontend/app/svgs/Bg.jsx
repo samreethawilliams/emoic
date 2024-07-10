ba812b40ea731e216b269eb2c8536d505f75fec0
samreethawilliams <samreethawilliams@gmail.com>
2024-07-10 11:25:34 -0400

    New Bg

--- frontend/app/svgs/Bg.jsx
+++ frontend/app/svgs/Bg.jsx
@@ -3,14 +3,9 @@ import { View, StyleSheet } from "react-native";
 import Svg, { Circle, LinearGradient, Stop, G } from "react-native-svg";
 
 const Bg = (props) => (
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    width="100%"
-    height="100%"
-    viewBox="0 0 800 800"
-  >
+  <svg xmlns="http://www.w3.org/2000/svg" {...props} viewBox="0 0 800 800">
     <rect fill="#C4E0E5" width="800" height="800" />
-    <g fill-opacity="0.29">
+    <g fillOpacity="0.29">
       <circle fill="#C4E0E5" cx="400" cy="400" r="600" />
       <circle fill="#aed3da" cx="400" cy="400" r="500" />
       <circle fill="#97c7cf" cx="400" cy="400" r="400" />
